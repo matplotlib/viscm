60d861271dd788a2dc8ad72d101b68a093963948
Nathaniel J. Smith <njs@pobox.com>
2015-11-03 21:58:57 -0800

    Bump version to 0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESC = open("README.rst").read()
 
 setup(
     name="viscm",
-    version="0.4",
+    version="0.5",
     description=DESC,
     long_description=LONG_DESC,
     author="Nathaniel J. Smith, Stefan van der Walt",
