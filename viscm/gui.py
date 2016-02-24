554f7508b5e521a7a1b0b02552028aaa2cb3f141
Hankun Zhao <jerryz123@berkeley.edu>
2016-02-23 23:22:20 -0800

    Fixed style errors

--- viscm/gui.py
+++ viscm/gui.py
@@ -149,9 +149,9 @@ def _vis_axes():
             'image1-cb': grid[2:5, 3],
             'image2': grid[5:8, 2],
             'image2-cb': grid[5:8, 3],
-            'image3' : grid[8:, 2],
-            'image3-cb' : grid[8:, 3]
-    }
+            'image3': grid[8:, 2],
+            'image3-cb': grid[8:, 3]
+            }
     axes = dict([(key, plt.subplot(value)) for (key, value) in axes.items()])
     axes['gamut'] = plt.subplot(grid[6:, :2], projection='3d')
     axes['gamut-toggle'] = plt.axes([0.01, 0.01, 0.08, 0.025])
@@ -326,9 +326,11 @@ class viscm(object):
         images.append(z)
         image_args.append({})
 
-        # Peter Kovesi's colormap test image at http://peterkovesi.com/projects/colourmaps/colourmaptest.asc
+        # Peter Kovesi's colormap test image at
+        # http://peterkovesi.com/projects/colourmaps/colourmaptest.asc
 
-        images.append(np.loadtxt(os.path.join(example_dir, "colourmaptest.txt")))
+        images.append(np.loadtxt(os.path.join(example_dir,
+                                              "colourmaptest.txt")))
 
         image_args.append({})
 
