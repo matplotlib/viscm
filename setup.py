188ef39a9f234d0bbbaa273b652c6e734a105389
Thomas A Caswell <tcaswell@gmail.com>
2019-08-26 21:50:05 -0400

    Release 0.9

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESC = open("README.rst").read()
 
 setup(
     name="viscm",
-    version="0.8",
+    version="0.9",
     description=DESC,
     long_description=LONG_DESC,
     author="Nathaniel J. Smith, Stefan van der Walt",
