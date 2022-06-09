b0516307330b35833cfac9f7b3c5ba683c555a9b
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-09 14:16:45 +0200

    Fix import of qt backend for matplotlib without qt4 support

--- viscm/gui.py
+++ viscm/gui.py
@@ -16,14 +16,11 @@ import numpy as np
 # Do this first before any other matplotlib imports, to force matplotlib to
 # use a Qt backend
 from matplotlib.backends.qt_compat import QtWidgets, QtCore, QtGui, _getSaveFileName
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas4
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas5
 
-def FigureCanvas(fig):
-    try:
-        return FigureCanvas5(fig)
-    except Exception:
-        return FigureCanvas4(fig)
+try:
+    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+except Exception:
+    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
 import matplotlib
 import matplotlib.pyplot as plt
